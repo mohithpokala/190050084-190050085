2215a414136f6f9bb8c68cfbc9a891585c829c79
Pokala Mohith <mohithpokala@cse.iitb.ac.in>
2020-10-17 11:54:03 +0530

    updated

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { FormComponent } from './form/form.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/contact', pathMatch: 'full' },
   { path: 'form', component: FormComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  {path:'**',redirectTo: '/contact',pathMatch:'full'}
 ];
 
 @NgModule({
