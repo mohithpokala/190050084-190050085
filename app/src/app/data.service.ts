b4a5c70c1b635d4c545b9572686d8a306a581454
Pokala Mohith <mohithpokala@cse.iitb.ac.in>
2020-10-18 15:07:01 +0530

    updated

--- app/src/app/data.service.ts
+++ app/src/app/data.service.ts
@@ -17,29 +17,19 @@ export class DataService {
     const url ='https://cs251-outlab-6.herokuapp.com/initial_values/';
     return this.http.get(url).pipe(
       catchError((err) => {
-        console.log('error caught in service')
         console.error(err);
-
-        //Handle the error here
-
-        return throwError(err);    //Rethrow it back to component
-        alert('Failed to get data');
+        return throwError(err);
       }
       ));
   }
 
   post(message:string): Observable<any> {
-    //return this.http.post<any>('https://cs251-outlab-6.herokuapp.com/add_new_feedback/', message, this.httpOptions);
     return this.http.post('https://cs251-outlab-6.herokuapp.com/add_new_feedback/', message).pipe(
       catchError((err) => {
-        console.log('error caught in service')
-        console.error(err);
 
-        //Handle the error here
         alert('Invalid data!! :-(');
-        return throwError(err);    //Rethrow it back to component
-        
+        return throwError(err); 
       }
-      ));;
+      ));
   }
 }
